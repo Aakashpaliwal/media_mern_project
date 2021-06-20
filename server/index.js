80648d0fe5a03eaed917ea53e66d56dcd76b1705
Aakashpaliwal <aakashpaliwal95@gmail.com>
2021-06-20 12:49:48 +0530

    env variable add

--- server/index.js
+++ server/index.js
@@ -2,10 +2,11 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import mongoose from "mongoose";
-
+import dotenv from "dotenv";
 import postRoutes from "./routes/posts.js";
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -16,12 +17,12 @@ app.use(cors());
 //routes
 app.use("/posts", postRoutes);
 //setup DB
-const CONNECTION_URL =
-  "mongodb+srv://aakash404:Barcelona@123@cluster0.u5wyo.mongodb.net/memories_db?retryWrites=true&w=majority";
+// const CONNECTION_URL =
+//   "mongodb+srv://aakash404:Barcelona@123@cluster0.u5wyo.mongodb.net/memories_db?retryWrites=true&w=majority";
 
 const PORT = process.env.PORT || 5000;
 mongoose
-  .connect(CONNECTION_URL, {
+  .connect(process.env.CONNECTION_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
